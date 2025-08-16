6a6cf5cd0309f8e304a8dc7c79a9d826b854fe4e
Kadir Ertan <kadirertan000@gmail.com>
2025-08-16 17:07:33 +0300

    remove AuthServiceProvider entry that added for test purpose from providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\AuthServiceProvider::class,
-    App\Providers\FortifyServiceProvider::class,
+  App\Providers\AppServiceProvider::class,
+  App\Providers\FortifyServiceProvider::class,
 ];
